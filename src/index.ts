492fac438e3c61d33153e7c84d3bead75ecf4948
Ashley Anderson <aganders3@gmail.com>
2024-08-20 19:45:10 -0400

    Force eslint error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { LayerManager } from "core/layer_manager";
 
 
+const unused = 0;
+
 export { SingleMeshLayer } from "layers/single_mesh_layer";
 
 export { WebGLRenderer } from "renderers/webgl_renderer";
