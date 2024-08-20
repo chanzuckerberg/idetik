b1626e1e590712599c4917bef09c49b7a097c62c
Ashley Anderson <aganders3@gmail.com>
2024-08-20 19:48:04 -0400

    Revert "Force eslint error"
    This reverts commit 492fac438e3c61d33153e7c84d3bead75ecf4948.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { LayerManager } from "core/layer_manager";
 
 
-const unused = 0;
-
 export { SingleMeshLayer } from "layers/single_mesh_layer";
 
 export { WebGLRenderer } from "renderers/webgl_renderer";
