8b08f431991e159c335ed4fc48ad1a9e4ef20dcc
Andy Sweet <andrew.d.sweet@gmail.com>
2024-10-02 11:08:44 -0400

    Update comment

--- ultrack-prototype/frontend/video_layer_props.ts
+++ ultrack-prototype/frontend/video_layer_props.ts
@@ -1,4 +1,6 @@
-// Source is 5D, so provide indices at 3 dimensions to project to 2D.
+// Source is 5D, so provide an interval in T and scalar indices
+// in C (first of three channels) and Z (first of only depth)
+// to get a 2D video.
 export const videoLayerTimeInterval = { start: 100, stop: 150 };
 export const videoLayerProps = {
   url: "https://public.czbiohub.org/royerlab/ultrack/multi-color/image.zarr/",
