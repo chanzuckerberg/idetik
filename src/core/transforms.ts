4380eae0818b2672ad3a37f2bf9fbc1a7bff1a6f
Ashley Anderson <aganders3@gmail.com>
2024-10-09 16:45:11 -0400

    Add rotation and scale to AffineTransform

--- src/core/transforms.ts
+++ src/core/transforms.ts
@@ -1,24 +1,39 @@
-import { mat4, vec3 } from "gl-matrix";
+import { mat4, vec3, quat } from "gl-matrix";
 
 export class AffineTransform {
   private dirty_ = true;
   private matrix_ = mat4.create();
   private inverse_: mat4 | null = null;
+  private rotation_ = quat.create();
   private translation_ = vec3.create();
+  private scale_ = vec3.fromValues(1, 1, 1);
+
+  public rotate(q: quat) {
+    quat.multiply(this.rotation_, this.rotation_, q);
+    vec3.transformQuat(this.translation_, this.translation_, q);
+    this.dirty_ = true;
+  }
 
   public translate(vec: vec3) {
     vec3.add(this.translation_, this.translation_, vec);
     this.dirty_ = true;
   }
 
-  // TODO: add scale
-
-  // TODO: add rotate
+  public scale(vec: vec3) {
+    vec3.multiply(this.scale_, this.scale_, vec);
+    vec3.multiply(this.translation_, this.translation_, vec);
+    this.dirty_ = true;
+  }
 
   public get matrix() {
     if (this.dirty_) {
       this.matrix_ = mat4.create();
-      mat4.translate(this.matrix_, this.matrix_, this.translation_);
+      mat4.fromRotationTranslationScale(
+        this.matrix_,
+        this.rotation_,
+        this.translation_,
+        this.scale_
+      );
       this.inverse_ = null;
       this.dirty_ = false;
     }
