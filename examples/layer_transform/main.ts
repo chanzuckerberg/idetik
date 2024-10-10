6d5363847e8248ad33a20bdde70980f64cbb5b4a
Ashley Anderson <aganders3@gmail.com>
2024-10-10 12:52:42 -0400

    Simplify layer transform example

--- examples/layer_transform/main.ts
+++ examples/layer_transform/main.ts
@@ -8,30 +8,52 @@ import {
 
 const layersManager = new LayerManager();
 
-const helixA = generateHelix({
-  radius: 1.0,
-  turns: 3.0,
-  pointsPerTurn: 100,
-  pitch: 1.5,
-  phase: 0.0,
-});
-
-const helixB = generateHelix({
-  radius: 1.0,
-  turns: 3.0,
-  pointsPerTurn: 100,
-  pitch: 1.5,
-  phase: 90.0,
-});
-
+const movingLayer = new ProjectedLineLayer([
+  {
+    path: [
+      [-1, 0, -5],
+      [0, -1, -5],
+      [1, 0, -5],
+      [0, 1, -5],
+      [-1, 0, -5],
+    ],
+    color: [0.7, 0.7, 0.0],
+    width: 0.1,
+  },
+]);
 const stationaryLayer = new ProjectedLineLayer([
-  { path: helixA, color: [1.0, 0.7, 0.0], width: 0.1 },
+  {
+    path: [
+      [-1, 0, -5],
+      [1, 0, -5],
+    ],
+    color: [0.7, 0.0, 0.0],
+    width: 0.1,
+  },
+  {
+    path: [
+      [0, -1, -5],
+      [0, 1, -5],
+    ],
+    color: [0.0, 0.7, 0.0],
+    width: 0.1,
+  },
+  {
+    path: [
+      [0, 0, -6],
+      [0, 0, -4],
+    ],
+    color: [0.0, 0.0, 0.7],
+    width: 0.1,
+  },
 ]);
-const transformedLayer = new ProjectedLineLayer([
-  { path: helixB, color: [0.0, 0.7, 0.0], width: 0.2 },
-]);
+stationaryLayer.transform.translate([0.0, 0.0, 5]);
+stationaryLayer.transform.rotate(
+  quat.fromEuler(quat.create(), 45.0, -45.0, 0.0)
+);
+stationaryLayer.transform.translate([0.0, 0.0, -5]);
 layersManager.add(stationaryLayer);
-layersManager.add(transformedLayer);
+layersManager.add(movingLayer);
 
 const renderer = new WebGLRenderer("#canvas");
 const camera = new PerspectiveCamera(60, renderer.width / renderer.height);
@@ -39,35 +61,13 @@ const camera = new PerspectiveCamera(60, renderer.width / renderer.height);
 animate();
 
 const rotate = () => {
-  transformedLayer.transform.rotate(
-    quat.fromEuler(quat.create(), 0.0, 0.0, 0.2)
-  );
+  movingLayer.transform.translate([0.0, 0.0, 5]);
+  movingLayer.transform.rotate(quat.fromEuler(quat.create(), 0.1, 0.2, 0.5));
+  movingLayer.transform.translate([0.0, 0.0, -5]);
 };
 setInterval(rotate, 5);
 
 function animate() {
   renderer.render(layersManager, camera);
   requestAnimationFrame(animate);
-}
-
-function generateHelix(params: {
-  radius: number;
-  turns: number;
-  pointsPerTurn: number;
-  pitch: number;
-  phase: number;
-}) {
-  const { radius, turns, pointsPerTurn, pitch, phase } = params;
-  const phaseRad = (phase * Math.PI) / 180;
-  const path: [number, number, number][] = [];
-  const totalPoints = turns * pointsPerTurn;
-
-  for (let i = 0; i < totalPoints; i++) {
-    const angle = (i / pointsPerTurn) * 2 * Math.PI;
-    const z = radius * Math.cos(angle + phaseRad) - 5.0;
-    const y = (i / pointsPerTurn) * pitch - (pitch * turns) / 2;
-    const x = radius * Math.sin(angle + phaseRad);
-    path.push([x, y, z]);
-  }
-  return path;
 }
