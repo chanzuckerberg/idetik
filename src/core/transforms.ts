168f17e80d16da06899cd47a6ec72584ef45554e
Ashley Anderson <aganders3@gmail.com>
2024-10-10 11:58:13 -0400

    Use same 'dirty_' pattern for both 'matrix' and 'inverse' properties

--- src/core/transforms.ts
+++ src/core/transforms.ts
@@ -1,9 +1,10 @@
 import { mat4, vec3, quat } from "gl-matrix";
 
 export class AffineTransform {
-  private dirty_ = true;
+  private dirtyMatrix_ = true;
+  private dirtyInverse_ = true;
   private matrix_ = mat4.create();
-  private inverse_: mat4 | null = null;
+  private inverse_ = mat4.create();
   private rotation_ = quat.create();
   private translation_ = vec3.create();
   private scale_ = vec3.fromValues(1, 1, 1);
@@ -11,38 +12,37 @@ export class AffineTransform {
   public rotate(q: quat) {
     quat.multiply(this.rotation_, this.rotation_, q);
     vec3.transformQuat(this.translation_, this.translation_, q);
-    this.dirty_ = true;
+    this.dirtyMatrix_ = true;
   }
 
   public translate(vec: vec3) {
     vec3.add(this.translation_, this.translation_, vec);
-    this.dirty_ = true;
+    this.dirtyMatrix_ = true;
   }
 
   public scale(vec: vec3) {
     vec3.multiply(this.scale_, this.scale_, vec);
     vec3.multiply(this.translation_, this.translation_, vec);
-    this.dirty_ = true;
+    this.dirtyMatrix_ = true;
   }
 
   public get matrix() {
-    if (this.dirty_) {
-      this.matrix_ = mat4.create();
+    if (this.dirtyMatrix_) {
       mat4.fromRotationTranslationScale(
         this.matrix_,
         this.rotation_,
         this.translation_,
         this.scale_
       );
-      this.inverse_ = null;
-      this.dirty_ = false;
+      this.dirtyMatrix_ = false;
+      this.dirtyInverse_ = true;
     }
     return this.matrix_;
   }
 
   public get inverse() {
-    if (this.dirty_ || this.inverse_ === null) {
-      this.inverse_ = mat4.invert(mat4.create(), this.matrix);
+    if (this.dirtyMatrix_ || this.dirtyInverse_) {
+      mat4.invert(this.inverse_, this.matrix);
     }
     return this.inverse_;
   }
