4b47c359716f4049db323e1db0c26eeb169af5b6
Andy Sweet <andrew.d.sweet@gmail.com>
2024-11-22 13:21:04 -0500

    Remove empty line

--- ultrack-prototype/frontend/lib/mock_data.ts
+++ ultrack-prototype/frontend/lib/mock_data.ts
@@ -1,7 +1,6 @@
 // TODO: this is a temporary store for hard-coded data. It will be replaced by
 // a more dynamic data source in the future.
 import { Answer, Task } from "./tasks";
-
 import tasks from "../../data/mock_data.json";
 
 // TODO: perhaps could be an async iterator or something
