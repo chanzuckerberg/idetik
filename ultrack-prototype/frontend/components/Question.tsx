08eb755a2c714346cd98a85762f96ddd6e57e5d5
Andy Sweet <andrew.d.sweet@gmail.com>
2024-11-25 10:23:29 -0500

    Justify shortcuts

--- ultrack-prototype/frontend/components/Question.tsx
+++ ultrack-prototype/frontend/components/Question.tsx
@@ -8,6 +8,9 @@ type Answer = {
   shortcut: string;
 };
 
+// Shortcuts "1", "2", and "3" are used to be ergonomic on most
+// keyboard layouts and to be consistent with Ultrack's napari plugin:
+// https://github.com/royerlab/ultrack/blob/ff3bf242e395bde3236153317802b5aff8dbb6a6/ultrack/validation/link_validation.py#L159
 const answers: Answer[] = [
   { type: "Yes", shortcut: "1" },
   { type: "No", shortcut: "2" },
