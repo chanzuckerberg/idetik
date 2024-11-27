67d77475cede6abe6c0001833b83e0ef496ec405
Andy Sweet <andrew.d.sweet@gmail.com>
2024-11-27 11:58:54 -0500

    Execute in submission order

--- src/data/task.ts
+++ src/data/task.ts
@@ -1,41 +1,53 @@
 export class TaskExecutor<T> {
   private readonly maxConcurrentTasks_: number;
-  private runningTasks_: Array<Promise<T>> = [];
+  private readonly runningTasks_: Array<Promise<T>> = [];
+  private readonly pendingTasks_: Array<() => Promise<T>> = [];
 
-  constructor(maxConcurrentTasks: number = 16) {
+  constructor(maxConcurrentTasks: number = 4) {
+    if (maxConcurrentTasks <= 0) {
+      throw Error(
+        `maxConcurrentTasks (${maxConcurrentTasks}) must be positive`
+      );
+    }
     this.maxConcurrentTasks_ = maxConcurrentTasks;
   }
 
-  async submit(task: Promise<T>): Promise<T> {
+  async submit(task: () => Promise<T>): Promise<T> {
+    this.pendingTasks_.push(task);
+    while (this.pendingTasks_[0] !== task) {
+      await this.waitUntilReady();
+    }
+    this.pendingTasks_.shift();
+    const promise = task();
+    this.runningTasks_.push(promise);
+    const result = await promise;
+    const index = this.runningTasks_.indexOf(promise);
+    this.runningTasks_.splice(index, 1);
+    return result;
+  }
+
+  private async waitUntilReady(): Promise<void> {
     while (this.runningTasks_.length >= this.maxConcurrentTasks_) {
       await Promise.race(this.runningTasks_);
     }
-    this.runningTasks_.push(task);
-    return task.then((result) => {
-      const index = this.runningTasks_.indexOf(task);
-      this.runningTasks_.splice(index, 1);
-      return result;
-    });
   }
 }
 
 export class TaskQueue<T> {
-  private pendingTasks_: Array<() => Promise<T>> = [];
+  private tasks_: Array<() => Promise<T>> = [];
   private executor_: TaskExecutor<T>;
 
   constructor(executor: TaskExecutor<T>) {
     this.executor_ = executor;
   }
 
-  add(pendingTask: () => Promise<T>): void {
-    this.pendingTasks_.push(pendingTask);
+  add(task: () => Promise<T>): void {
+    this.tasks_.push(task);
   }
 
   async onIdle(): Promise<T[]> {
     return await Promise.all(
-      this.pendingTasks_.map(async (pendingTask) => {
-        return await this.executor_.submit(pendingTask());
-      })
+      this.tasks_.map((task) => this.executor_.submit(task))
     );
   }
 }
