74176c9b708e0d31e6f51ab6bac1841f7a14a13a
Andy Sweet <andrew.d.sweet@gmail.com>
2024-12-13 15:51:42 -0500

    Add a couple of tests

---
+++ test/task.test.ts
@@ -0,0 +1,46 @@
+import { TaskExecutor } from "@/data/task";
+import { expect, test } from "vitest";
+
+test("TaskExecutor::TasksExecuteInOrder", async () => {
+  const executor = new TaskExecutor(2);
+  let blocked = true;
+  const promises = [];
+  for (let i = 0; i < 8; ++i) {
+    promises.push(
+      executor.submit(async () => {
+        const value = i;
+        while (blocked) {
+          await new Promise((resolve) => setTimeout(resolve, 10));
+        }
+        return value;
+      })
+    );
+  }
+
+  blocked = false;
+  const results = await Promise.all(promises);
+
+  expect(results).toEqual([...Array(8).keys()]);
+});
+
+test("TaskExecutor::ClearCancelsPendingTasks", async () => {
+  const executor = new TaskExecutor(1);
+  let blocked = true;
+  const promise0 = executor.submit(async () => {
+    while (blocked) {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+    return 0;
+  });
+  const promise1 = executor.submit(async () => {
+    return 1;
+  });
+
+  executor.clear();
+
+  blocked = false;
+  const result0 = await promise0;
+  const result1 = await promise1;
+  expect(result0).toEqual(0);
+  expect(result1).toBeUndefined();
+});
