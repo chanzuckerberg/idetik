7030ad2a2858201c391c81b22c3e6714a1b64b76
Andy Sweet <andrew.d.sweet@gmail.com>
2025-01-07 10:12:54 -0500

    Generate image URL from plate and well metadata

--- examples/organelle_box/main.ts
+++ examples/organelle_box/main.ts
@@ -6,16 +6,24 @@ import {
   OmeZarrImageSource,
 } from "@";
 import { PanZoomControls } from "@/objects/cameras/controls";
+import { loadOmeZarrPlate, loadOmeZarrWell } from "@/data/ome_zarr_hcs_metadata_loader";
 
 // From https://zenodo.org/records/11262587
-// const url = "http://127.0.0.1:8081/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr/";
-const url = "http://127.0.0.1:8081/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr/B/03/0/";
+const plateUrl = "http://127.0.0.1:8081/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr";
+const plate = await loadOmeZarrPlate(plateUrl);
+console.debug("plate", plate);
+const wellPath = plate.plate.wells[0].path;
+const well = await loadOmeZarrWell(plateUrl, wellPath);
+console.debug("well", well);
+const imagePath = well.well.images[0].path;
+const imageUrl = plateUrl + "/" + wellPath + "/" + imagePath;
+
 const layerManager = new LayerManager();
 const renderer = new WebGLRenderer("#canvas");
 const camera = new OrthographicCamera(0, 1920, 0, 1440, 0, 10000);
 const controls = new PanZoomControls(camera, camera.position);
 renderer.setControls(controls);
-const source = new OmeZarrImageSource(url);
+const source = new OmeZarrImageSource(imageUrl);
 const region = [
   { dimension: "c", index: 0 },
   { dimension: "z", index: 0 },
