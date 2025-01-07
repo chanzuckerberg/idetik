512e0404f094ebd4210133332450b0c1a7ece6db
Andy Sweet <andrew.d.sweet@gmail.com>
2025-01-07 11:30:49 -0500

    Add well and image selectors

--- examples/organelle_box/main.ts
+++ examples/organelle_box/main.ts
@@ -16,24 +16,58 @@ const plateUrl =
   "http://127.0.0.1:8081/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr";
 const plate = await loadOmeZarrPlate(plateUrl);
 console.debug("plate", plate);
-const wellPath = plate.plate.wells[0].path;
-const well = await loadOmeZarrWell(plateUrl, wellPath);
-console.debug("well", well);
-const imagePath = well.well.images[0].path;
-const imageUrl = plateUrl + "/" + wellPath + "/" + imagePath;
+
+const wellPaths = plate.plate.wells.map((well) => well.path);
+const wellSelector = document.querySelector("#well") as HTMLSelectElement;
+wellPaths.forEach((path) => {
+  const option = document.createElement("option");
+  option.value = path;
+  option.text = path;
+  wellSelector.appendChild(option);
+});
 
 const layerManager = new LayerManager();
 const renderer = new WebGLRenderer("#canvas");
 const camera = new OrthographicCamera(0, 1920, 0, 1440);
 const controls = new PanZoomControls(camera, camera.position);
 renderer.setControls(controls);
-const source = new OmeZarrImageSource(imageUrl);
 const region = [
   { dimension: "c", index: 0 },
   { dimension: "z", index: 0 },
 ];
-const layer = new ImageLayer(source, region);
-layerManager.add(layer);
+
+const imageSelector = document.querySelector("#image") as HTMLSelectElement;
+
+const onImageChange = async () => {
+  console.debug("onImageChange: ", imageSelector.value);
+  layerManager.layers.length = 0;
+  const imageUrl =
+    plateUrl + "/" + wellSelector.value + "/" + imageSelector.value;
+  const source = new OmeZarrImageSource(imageUrl);
+  const layer = new ImageLayer(source, region);
+  layerManager.add(layer);
+};
+
+const onWellChange = async () => {
+  console.debug("onWellChange: ", wellSelector.value);
+  layerManager.layers.length = 0;
+  const path = wellSelector.value;
+  const well = await loadOmeZarrWell(plateUrl, path);
+  console.debug("well", well);
+  const imagePaths = well.well.images.map((image) => image.path);
+  imageSelector.innerHTML = "";
+  imagePaths.forEach((path) => {
+    const option = document.createElement("option");
+    option.value = path;
+    option.text = path;
+    imageSelector.appendChild(option);
+  });
+  await onImageChange();
+};
+
+wellSelector.addEventListener("change", onWellChange);
+imageSelector.addEventListener("change", onImageChange);
+await onWellChange();
 
 function animate() {
   renderer.render(layerManager, camera);
