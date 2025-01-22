b047ddaa7e2a253a8ca8df4b035cd8a11b837b41
Andy Sweet <andrew.d.sweet@gmail.com>
2025-01-22 10:57:50 -0500

    Add explicit pixel scale to example

--- examples/image2d_from_omezarr3d_f32/main.ts
+++ examples/image2d_from_omezarr3d_f32/main.ts
@@ -9,12 +9,13 @@ import { PanZoomControls } from "@/objects/cameras/controls";
 
 const url =
   "https://files.cryoetdataportal.cziscience.com/10444/24apr23a_Position_12/Reconstructions/VoxelSpacing4.990/Tomograms/100/24apr23a_Position_12.zarr";
+const pixelScale = 4.99;
 const layerManager = new LayerManager();
 const renderer = new WebGLRenderer("#canvas");
-const camera = new OrthographicCamera(0, 19.96 * 316, 0, 19.96 * 316);
+const camera = new OrthographicCamera(0, pixelScale * 1264, 0, pixelScale * 1264);
 const controls = new PanZoomControls(camera, camera.position);
 renderer.setControls(controls);
-const region = [{ dimension: "z", index: 0.5 * 19.96 * 93 }];
+const region = [{ dimension: "z", index: pixelScale * 0.5 * 370}];
 
 const source = new OmeZarrImageSource(url);
 const layer = new ImageLayer(source, region);
