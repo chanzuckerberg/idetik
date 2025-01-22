999ea1a2c32bb25f985ea431bdda6691b014cdfc
Andy Sweet <andrew.d.sweet@gmail.com>
2025-01-22 10:47:32 -0500

    Add explanatory comment

--- src/renderers/shaders/float_image_frag.glsl
+++ src/renderers/shaders/float_image_frag.glsl
@@ -9,8 +9,10 @@ uniform mediump sampler2D texture0;
 in vec2 TexCoords;
 
 void main() {
-    // TODO: normalization of the value should be controlled by variable
-    // parameters (e.g. contrast limits).
+    // The scaling factor gives the float32 CryoET example data some
+    // contrast when rendered.
+    // TODO: instead normalization of the value should be controlled by
+    // variable parameters (e.g. contrast limits).
     // https://github.com/chanzuckerberg/imaging-active-learning/issues/32
     float value = 1e5 * texture(texture0, TexCoords).r;
     fragColor = vec4(value, value, value, 1);
