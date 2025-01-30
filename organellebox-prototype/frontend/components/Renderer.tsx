b80e2240d265b3dcde031979314a91f4123c1da4
Ashley Anderson <aganders3@gmail.com>
2025-01-29 19:55:50 -0500

    Simplest hard-coded viewer component

--- organellebox-prototype/frontend/components/Renderer.tsx
+++ organellebox-prototype/frontend/components/Renderer.tsx
@@ -1,22 +1,45 @@
 import { useEffect } from "react";
 import {
-  // ImageLayer,
+  ImageLayer,
+  OmeZarrImageSource,
   LayerManager,
   OrthographicCamera,
   WebGLRenderer,
 } from "@";
 
-import { Box } from "@mui/material";
-import { NullControls, PanZoomControls } from "@/objects/cameras/controls";
+import {
+  loadOmeZarrPlate,
+  loadOmeZarrWell,
+} from "@/data/ome_zarr_hcs_metadata_loader";
 
 const canvasId = "canvas";
 
-// TODO: consider useRef for these objects
+// TODO: useRef for some of these objects
 const camera = new OrthographicCamera(0, 1920, 0, 1440);
-const controls = new PanZoomControls(camera, camera.position);
 const layerManager = new LayerManager();
-// const imageLayer = new ImageLayer(source, region);
-// layerManager.addLayer(imageLayer);
+
+// TODO: use props to pass in most of this config
+const plateUrl =
+  "http://localhost:8000/20200812-CardiomyocyteDifferentiation14-Cycle1_mip.zarr";
+const plate = await loadOmeZarrPlate(plateUrl);
+const wellPaths = plate.plate?.wells.map((well) => well.path);
+if (!wellPaths) {
+  throw new Error("No wells found in plate");
+}
+const well = await loadOmeZarrWell(plateUrl, wellPaths[0]);
+const imagePaths = well.well?.images.map((image) => image.path);
+if (!imagePaths) {
+  throw new Error("No images found in well");
+}
+const imageUrl = plateUrl + "/" + wellPaths[0] + "/" + imagePaths[0];
+const source = new OmeZarrImageSource(imageUrl);
+const region = [
+  { dimension: "c", index: 0 },
+  { dimension: "z", index: 0 },
+];
+const layer = new ImageLayer(source, region);
+layerManager.add(layer);
+
 
 export default function Renderer() {
   // Use the mount-effect so that the renderer can find the corresponding
@@ -25,7 +48,6 @@ export default function Renderer() {
     console.debug("Renderer::mount");
     let lastRequestId = 0;
     const renderer = new WebGLRenderer(`#${canvasId}`);
-    renderer.setControls(controls);
     function animate() {
       renderer.render(layerManager, camera);
       lastRequestId = requestAnimationFrame(animate);
@@ -33,7 +55,6 @@ export default function Renderer() {
     animate();
     return () => {
       // TODO: cleanup by disposing objects owned by the renderer and camera.
-      renderer.setControls(new NullControls());
       if (lastRequestId > 0) {
         console.debug(`Cancelling animation frame ${lastRequestId}`);
         cancelAnimationFrame(lastRequestId);
@@ -42,16 +63,6 @@ export default function Renderer() {
   }, []);
 
   return (
-    <Box
-      sx={{
-        flex: 1,
-        minHeight: 0,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <canvas id={canvasId} style={{ width: "100%", height: "100%" }} />
-    </Box>
+    <canvas id={canvasId} style={{ width: "100%", height: "100%" }} />
   );
 }
