76da16229328dec2f23ee8ad65256ef89d23ba26
Jessica Gadling <jessica@mootech.net>
2025-01-30 13:26:54 -0800

    Zarr typing/validation via zod (#156)
    ### Summary
    This is another version of codegen'ing types and validations, this time
    using the zod library. Imop this is the best of the bunch I've worked
    with (ava, quicktype, and zod)
    
    Zod implements value validation, generates very nice simple declarative
    code, and generally behaves nicely with the TS ecosystem by raising
    exceptions when it runs into trouble with validation.
    
    ### Related Issue
    Closes #<issue-number>
    
    ### Tests & Checks
    <!---
      How did you validate that your changes were implemented correctly?
      
      Please think about the following prompts:
      1. I wrote automated tests.
      2. I manually tested my changes, and here's what I did:
    -->

--- src/data/ome_zarr_hcs_metadata_loader.ts
+++ src/data/ome_zarr_hcs_metadata_loader.ts
@@ -1,73 +1,12 @@
 import * as zarr from "zarrita";
-
-// Plate metadata from OME-NGFF v0.4:
-// https://ngff.openmicroscopy.org/0.4/#plate-md
-type PlateAcquisition = {
-  // >= 0
-  id: number;
-  name?: string;
-  description?: string;
-  // > 0, maximum number of fields of view for the acquisition
-  maximumfieldcount?: number;
-  // integer epoch timestamps
-  starttime?: number;
-  endtime?: number;
-};
-
-type PlateColumn = {
-  // alphanumeric characters only, case sensitive, unique across all columns in plate
-  name: string;
-};
-
-type PlateRow = {
-  // alphanumeric characters only, case sensitive, unique across all rows in plate
-  name: string;
-};
-
-type PlateWell = {
-  // must be of the form {row}/{column} where {row} is a row name and {column} is a column name
-  // must not contain additional leading or trailing directories
-  path: string;
-  // 0-based indices into the plate's rows and columns
-  rowIndex: number;
-  columnIndex: number;
-};
-
-export type Plate = {
-  plate: {
-    columns: PlateColumn[];
-    rows: PlateRow[];
-    wells: PlateWell[];
-    name?: string;
-    version?: string;
-    // > 0, number of fields per view across all wells
-    field_count?: number;
-    acquisitions?: PlateAcquisition[];
-  };
-};
-
-// Well metadata from OME-NGFF v0.4:
-// https://ngff.openmicroscopy.org/0.4/#well-md
-type WellImage = {
-  // alphanumeric characters only, case sensitive, unique within the well image paths
-  path: string;
-  // present if multiple acquisitions were performed in the plate
-  // identifies the acquisition from the plate acquisition metadata
-  acquisition?: number;
-};
-
-export type Well = {
-  well: {
-    images: WellImage[];
-    version?: string;
-  };
-};
+import { Plate } from "data/zarr/0.4/plate";
+import { Well } from "data/zarr/0.4/well";
 
 export async function loadOmeZarrPlate(url: string): Promise<Plate> {
   const store = new zarr.FetchStore(url);
   const group = await zarr.open.v2(store, { kind: "group" });
-  // TODO: validate attributes
-  return group.attrs as Plate;
+  // Will throw validation exceptions that we can catch if we want.
+  return Plate.parse(group.attrs);
 }
 
 export async function loadOmeZarrWell(
@@ -76,6 +15,7 @@ export async function loadOmeZarrWell(
 ): Promise<Well> {
   const store = new zarr.FetchStore(url + "/" + path);
   const root = await zarr.open.v2(store, { kind: "group" });
-  // TODO: validate attributes
-  return root.attrs as Well;
+
+  // Will throw validation exceptions that we can catch if we want.
+  return Well.parse(root.attrs);
 }
