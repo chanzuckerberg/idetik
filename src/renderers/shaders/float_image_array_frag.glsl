50b43b0c2195e9908a7e7342cda15b5ef230fbd2
Andy Sweet <andrew.d.sweet@gmail.com>
2025-01-31 10:27:55 -0500

    Fix hcs example

--- src/renderers/shaders/float_image_array_frag.glsl
+++ src/renderers/shaders/float_image_array_frag.glsl
@@ -5,6 +5,7 @@ precision mediump float;
 layout (location = 0) out vec4 fragColor;
 
 uniform mediump sampler2DArray texture0;
+uniform bool Visible[3];
 uniform vec3 Color[3];
 uniform float ValueOffset[3];
 uniform float ValueScale[3];
@@ -14,6 +15,7 @@ in vec2 TexCoords;
 void main() {
     vec3 rgbColor = vec3(0, 0, 0);
     for (int i = 0; i < 3; i++) {
+        if (!Visible[i]) continue;
         float texel = texture(texture0, vec3(TexCoords, i)).r;
         float value = (texel + ValueOffset[i]) * ValueScale[i];
         rgbColor += value * Color[i].rgb;
