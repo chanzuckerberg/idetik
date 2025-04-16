e8b468c85559b6cc5a867d5075427b1c5175be17
Kyle Awayan <kyle@awayan.com>
2025-04-16 11:46:34 -0700

    style sliders

--- packages/react/src/components/viewers/OmeZarrImageViewer/components/ChannelControlsList/components/ChannelControl/components/ContrastSlider/ContrastSlider.tsx
+++ packages/react/src/components/viewers/OmeZarrImageViewer/components/ChannelControlsList/components/ChannelControl/components/ContrastSlider/ContrastSlider.tsx
@@ -37,12 +37,27 @@ export function ContrastSlider({
         // Hardcode dark mode colors to force dark mode
         // look, no matter what the theme is
         "[&_.MuiSlider-rail]:!bg-[#494949]",
-        "[&_.MuiSlider-track]:!bg-[#0D7CB5]",
+        "[&_.MuiSlider-track]:!bg-dark-sds-color-primitive-blue-400",
         "[&_.MuiSlider-mark]:!bg-[#696969]",
-        "[&_.MuiSlider-thumb]:after:!bg-[#101010]",
-        "[&_.MuiSlider-valueLabel]:!bg-[#065B86]",
+        "[&_.MuiSlider-valueLabel]:!bg-dark-sds-color-primitive-blue-400",
         "[&_.MuiSlider-valueLabelLabel]:!text-white"
       )}
+      sx={{
+        // Hacky way to override the thumb circle color to white
+        // even in dark mode.
+        // Higher specificity selectors to override !important styles
+        "&&& .MuiSlider-thumb": {
+          "&:after": {
+            backgroundColor: "#fff !important",
+          },
+        },
+        // Alternative approach with direct element + class targeting for even higher specificity
+        "span.MuiSlider-thumb": {
+          "&:after": {
+            backgroundColor: "#fff !important",
+          },
+        },
+      }}
     />
   );
 }
