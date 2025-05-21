8800bdc32bf5ce22d3987c76b619ab5622ffdf83
Kyle Awayan <kyle@awayan.com>
2025-05-21 11:11:09 -0700

    remove number padding

--- packages/react/src/components/viewers/OmeZarrImageViewer/OmeZarrImageViewer.tsx
+++ packages/react/src/components/viewers/OmeZarrImageViewer/OmeZarrImageViewer.tsx
@@ -97,8 +97,7 @@ export function OmeZarrImageViewer(props: OmeZarrViewerContainerProps) {
               classNames?.sliceIndicator
             )}
           >
-            Slice {zIndex.toString().padStart(2, "0")}/
-            {(zRange[1] - zRange[0]).toString().padStart(2, "0")}
+            Slice {zIndex}/{zRange[1] - zRange[0]}
           </div>
         ) : (
           <LoadingIndicator sdsStyle="tag" />
