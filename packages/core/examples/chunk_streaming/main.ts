192c60c730484ef13662a2af7ca1f202a043834a
Shlomi Nissan <admin@betamark.com>
2025-06-05 15:05:46 -0700

    Add chunk streaming example sandbox (#270)
    Summary:
    
    This PR introduces a new example demonstrating image loading via chunk
    streaming. It also includes a sandboxed copy of ImageLayer, which we’ll
    use to prototype chunk streaming support—such as integrating with the chunk
    manager and generating per-tile image renderables. This isolated setup allows
    us to iterate on chunk streaming features safely without impacting existing
    examples or functionality.

---
+++ packages/core/examples/chunk_streaming/main.ts
@@ -0,0 +1,36 @@
+import {
+  Idetik,
+  ImageLayerXYZ,
+  OmeZarrImageSource,
+  OrthographicCamera,
+  Region,
+} from "@";
+import { PanZoomControls } from "@/objects/cameras/controls";
+
+const url =
+  "https://public.czbiohub.org/royerlab/zebrahub/imaging/single-objective/ZSNS001.ome.zarr/";
+const left = 150;
+const right = 950;
+const top = 100;
+const bottom = 900;
+
+// Source is 5D, so provide indices at 3 dimensions to project to 2D.
+// Also specify a subregion in x and y to exercise that part of the API.
+const source = new OmeZarrImageSource(url);
+const region: Region = [
+  { dimension: "t", index: { type: "point", value: 400 } },
+  { dimension: "c", index: { type: "point", value: 0 } },
+  { dimension: "z", index: { type: "point", value: 300 } },
+  { dimension: "y", index: { type: "interval", start: top, stop: bottom } },
+  { dimension: "x", index: { type: "interval", start: left, stop: right } },
+];
+const channelProps = [{ contrastLimits: [0, 255] as [number, number] }];
+const layer = new ImageLayerXYZ({ source, region, channelProps });
+const camera = new OrthographicCamera(left, right, top, bottom);
+
+new Idetik({
+  canvasSelector: "canvas",
+  camera,
+  controls: new PanZoomControls(camera, camera.position),
+  layers: [layer],
+}).start();
