8df5d18534d53e3c27e28e49efd8a773548bd73b
Andy Sweet <andrew.d.sweet@gmail.com>
2025-06-24 12:04:48 -0400

    Stop listening for mouse events on leave/out

--- packages/core/src/objects/cameras/controls.ts
+++ packages/core/src/objects/cameras/controls.ts
@@ -87,10 +87,14 @@ export class PanZoomControls implements CameraControls {
     const onMouseUp = () => {
       target.removeEventListener("mousemove", onMouseMove);
       target.removeEventListener("mouseup", onMouseUp);
+      target.removeEventListener("mouseleave", onMouseUp);
+      target.removeEventListener("mouseout", onMouseUp);
     };
 
     target.addEventListener("mousemove", onMouseMove);
     target.addEventListener("mouseup", onMouseUp);
+    target.addEventListener("mouseleave", onMouseUp);
+    target.addEventListener("mouseout", onMouseUp);
   }
 
   private pan(deltaWorld: vec3) {
