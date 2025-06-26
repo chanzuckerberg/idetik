321f4e490b6fb829d95038da97fb34aaca6fb366
Ashley Anderson <aganders3@gmail.com>
2025-06-26 10:45:11 -0400

    Add IdetikCanvas component

---
+++ packages/react/src/components/IdetikCanvas.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useCallback } from "react";
+import { useIdetik } from "./hooks/useIdetik";
+
+interface IdetikCanvasProps {
+  canvasId?: string;
+  style?: React.CSSProperties;
+}
+
+export function IdetikCanvas({
+  canvasId = "idetik-canvas",
+  style = { width: "100%", height: "100%" },
+}: IdetikCanvasProps) {
+  const contextValue = useIdetik();
+
+  const canvasCallbackRef = useCallback(
+    (canvas: HTMLCanvasElement | null) => {
+      if (!contextValue.isReady && canvas) {
+        contextValue.initializeWithCanvas(canvas);
+      }
+    },
+    [contextValue]
+  );
+
+  return (
+    <canvas
+      ref={canvasCallbackRef}
+      id={canvasId}
+      className="idetik-canvas"
+      style={style}
+    />
+  );
+}
---
+++ packages/react/src/components/index.ts
@@ -0,0 +1,13 @@
+// Core components
+export { default as App } from "./App";
+export { default as AppWithProviders } from "./AppWithProviders";
+export { IdetikCanvas } from "./IdetikCanvas";
+
+// Hooks
+export * from "./hooks";
+
+// Providers
+export * from "./providers";
+
+// Viewers
+export * from "./viewers";
