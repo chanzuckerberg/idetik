193ed2736d02760ba8047518a3693c497da596e9
Andy Sweet <andrew.d.sweet@gmail.com>
2025-08-01 15:42:45 -0400

    Clean up

--- packages/core/src/data/ome_ngff/index.ts
+++ packages/core/src/data/ome_ngff/index.ts
@@ -6,6 +6,24 @@ import { Image } from "./0.5/image";
 import { Plate } from "./0.5/plate";
 import { Well } from "./0.5/well";
 
+const versions = ["0.4", "0.5"] as const;
+const versionsSet: ReadonlySet<string> = new Set(versions);
+type Version = (typeof versions)[number];
+
+function getVersion(attrs: object): Version {
+  // From v0.5 onwards, we assume that ome.version indicates the version.
+  // If it is not present or malformed, we assume it is v0.4, which is
+  // the oldest format we support.
+  const fallback = "0.4"
+  if (!("ome" in attrs)) return fallback;
+  if (!(attrs.ome instanceof Object)) return fallback;
+  const ome = attrs.ome;
+  if (!("version" in ome)) return fallback;
+  if (typeof ome.version !== "string") return fallback;
+  if (!versionsSet.has(ome.version)) return fallback;
+  return ome.version as Version;
+}
+
 export async function loadOmeZarrPlate(url: string): Promise<Plate> {
   const store = new zarr.FetchStore(url);
   const group = await zarr.open(store, { kind: "group" });
@@ -40,49 +58,6 @@ export async function loadOmeroDefaultZ(url: string): Promise<number> {
   return group.attrs?.omero?.rdefs?.defaultZ ?? 0;
 }
 
-const versions = ["0.4", "0.5"] as const;
-type Version = (typeof versions)[number];
-
-type attrsWithVersion = {
-  version: Version;
-};
-
-function hasVersion(ome: object): ome is attrsWithVersion {
-  return "version" in ome && versions.includes(ome.version as Version);
-}
-
-function getVersion(ome: object) {
-  if (hasVersion(ome)) {
-    return ome.version;
-  }
-}
-
-function isImageV04(image: object): image is ImageV04 {
-  // In v0.4, the version is buried in a multiscales array element and
-  // is optional, so it's easier to just check for the multiscales array.
-  return "multiscales" in image && Array.isArray(image.multiscales);
-}
-
-type imageWithOmeObject = {
-  ome: object;
-};
-
-function hasOmeObject(image: object): image is imageWithOmeObject {
-  return "ome" in image && typeof image.ome === "object";
-}
-
-function getOmeObject(image: object) {
-  if (hasOmeObject(image)) {
-    return image.ome;
-  }
-}
-
-function isImageV05(image: object): image is Image {
-  const ome = getOmeObject(image);
-  if (ome === undefined) return false;
-  return getVersion(ome) !== "0.5";
-}
-
 function adaptImageV04(imagev04: ImageV04): Image {
   return {
     ome: {
@@ -98,45 +73,18 @@ type NormalizedImage = {
   originalVersion: Version;
 };
 
-function normalizeImage(image: object): NormalizedImage {
-  if (isImageV04(image)) {
-    return {
-      image: adaptImageV04(image),
-      originalVersion: "0.4",
-    };
+export function parseOmeNgffImage(image: object): NormalizedImage {
+  const version = getVersion(image);
+  switch (version) {
+    case "0.5":
+      return {
+        image: Image.parse(image),
+        originalVersion: "0.5",
+      };
+    case "0.4":
+      return {
+        image: adaptImageV04(ImageV04.parse(image)),
+        originalVersion: "0.4",
+      }
   }
-  if (isImageV05(image)) {
-    return {
-      image,
-      originalVersion: "0.5",
-    };
-  }
-  throw new Error("Unknown image version");
-}
-
-export function parseOmeNgffImage(
-  group: zarr.Group<zarr.FetchStore>
-): NormalizedImage {
-  // copy attrs to avoid mutating the original
-  const attrs = { ...group.attrs };
-
-  const { image, originalVersion } = normalizeImage(attrs);
-
-  // TODO: silly fix for removing top-level identity transform,
-  // which is not allowed by spec but may have been written by
-  // some writers.
-  // This may need to be done for top-level `coordinateTransformations` as well.
-  // https://github.com/ome/ngff/pull/152
-  if (
-    Array.isArray(image.ome.multiscales) &&
-    Array.isArray(image.ome.multiscales[0]?.coordinateTransformations) &&
-    image.ome.multiscales[0].coordinateTransformations[0]?.type === "identity"
-  ) {
-    delete image.ome.multiscales[0].coordinateTransformations;
-  }
-
-  return {
-    image: Image.parse(image),
-    originalVersion: originalVersion,
-  };
 }
