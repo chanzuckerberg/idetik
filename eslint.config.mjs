b3de38adfc4a325fd55808aecabbfc65279a8705
Andy Sweet <andrew.d.sweet@gmail.com>
2025-08-04 15:29:34 -0400

    Add cjs lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,47 +1,64 @@
 import globals from "globals";
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default [
-  { files: ["**/*.{js,mjs,jsx,cjs,ts,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
   {
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    }
+    ignores: ["node_modules/", "**/dist/", "**/coverage/"],
   },
+
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+
   {
+    files: ["**/*.{js,mjs,jsx,ts,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
     rules: {
-      'no-duplicate-imports': 'error',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
+      "no-duplicate-imports": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
         {
-          "args": "all",
-          "argsIgnorePattern": "^_",
-          "destructuredArrayIgnorePattern": "^_",
-          "varsIgnorePattern": "^_"
-        }
+          args: "all",
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
       ],
-      '@typescript-eslint/naming-convention': [
-        'error',
+      "@typescript-eslint/naming-convention": [
+        "error",
         {
-          "selector": "classProperty",
-          "modifiers": ["private"],
-          "format": null,
-          "suffix": ["_"]
-        }
+          selector: "classProperty",
+          modifiers: ["private"],
+          format: null,
+          suffix: ["_"],
+        },
       ],
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
-    }
+    },
   },
-  { ignores: ["node_modules", "coverage", "**/dist"] },
+
+  {
+    files: ["**/*.cjs"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-var-requires": "off",
+    },
+  },
 ];
