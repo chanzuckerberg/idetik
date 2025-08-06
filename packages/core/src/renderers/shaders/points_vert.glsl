7f07220a618bb5ee725397e4ed2f2d4de541ed18
Ashley Anderson <aganders3@gmail.com>
2025-08-06 12:29:07 -0400

    Add support for color + layer transparency in Points renderable

--- packages/core/src/renderers/shaders/points_vert.glsl
+++ packages/core/src/renderers/shaders/points_vert.glsl
@@ -3,7 +3,7 @@
 precision mediump float;
 
 layout (location = 0) in vec3 inPosition;
-layout (location = 7) in vec3 inColor;
+layout (location = 7) in vec4 inColor;
 layout (location = 8) in float inSize;
 layout (location = 9) in float inMarker;
 
@@ -17,7 +17,7 @@ flat out uint marker;
 void main() {
     gl_Position = Projection * ModelView * vec4(inPosition, 1.0);
     gl_PointSize = inSize;
-    color = vec4(inColor, 1.0);
+    color = inColor;
     marker = uint(inMarker);
 }
 
