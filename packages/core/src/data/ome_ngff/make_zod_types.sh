e1a6da7517b35e827db178cc069959c6e2f804eb
Andy Sweet <andrew.d.sweet@gmail.com>
2025-08-06 15:19:13 -0400

    Use selective URLs instead

--- packages/core/src/data/ome_ngff/make_zod_types.sh
+++ packages/core/src/data/ome_ngff/make_zod_types.sh
@@ -1,20 +1,28 @@
-#! /bin/zsh
+#! /bin/bash
 
-version=0.4
-root_dir=/tmp/ngff
-
-if [ ! -d "${root_dir}" ]; then
-    echo "Cloning OME-NGFF repository..."
-    git clone git@github.com:ome/ngff.git --recursive ${root_dir}
-else
-    echo "Using existing OME-NGFF repository at ${root_dir}."
+if [ "$#" -lt 2 ]; then
+    echo "Error: Please provide the version number and at least one schema name." >&2
+    echo "Usage: $0 version [schema1 schema 2...]" >&2
+    exit 1
 fi
 
-schema_dir="${root_dir}/${version}/schemas"
-output_dir="./${version}"
+script_dir=$(dirname "$0")
+version="$1"
+schemas=( "${@:2}" )
+
+echo "Generating zod types for OME-Zarr v${version} for schemas: ${schemas[*]}"
+
+base_url="https://ngff.openmicroscopy.org/${version}/schemas"
+
+resolver="${script_dir}/resolve_schema.cjs"
+
+output_dir="${script_dir}/${version}"
 mkdir -p "${output_dir}"
 
-for i in $(ls ${schema_dir}/*.schema | grep -v strict | xargs basename | cut -d '.' -f 1); do 
-    echo "Processing schema: ${schema_dir}/${i}.schema"
-    node resolve_schema.cjs "${schema_dir}/${i}.schema" | json-schema-to-zod -o "${output_dir}/${i}.ts" --name ${(C)i} --withJsdocs --type ${(C)i};
+for s in "${schemas[@]}"; do
+    url="${base_url}/${s}.schema"
+    class_name=$(echo "$s" | awk '{print toupper(substr($0,1,1)) substr($0,2)}')
+    output_file="${output_dir}/${s}.ts"
+    echo "Processing schema: ${url} to ${output_file} as class ${class_name}"
+    node "${resolver}" "${url}" | json-schema-to-zod -o "${output_file}" --name "${class_name}" --withJsdocs --type "${class_name}"
 done
