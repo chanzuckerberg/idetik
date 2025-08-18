1c0406ea3cff42abf316028fb2a7944101c7bd2a
Andy Sweet <andrew.d.sweet@gmail.com>
2025-08-18 12:56:25 -0400

    Remove log

--- packages/core/src/data/ome_zarr/image_loader.ts
+++ packages/core/src/data/ome_zarr/image_loader.ts
@@ -59,8 +59,6 @@ export class OmeZarrImageLoader {
     const array = this.arrays_[chunk.lod];
     const attrs = this.loaderAttributes_[chunk.lod];
 
-    console.debug("loadChunkData", chunk);
-
     const chunkCoords: number[] = [];
     chunkCoords[mapping.x.sourceIndex] = chunk.chunkIndex.x;
     chunkCoords[mapping.y.sourceIndex] = chunk.chunkIndex.y;
