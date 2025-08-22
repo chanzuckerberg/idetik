819d4a8925781b4fa9a4f27bcd5448595c1f32f4
Shlomi Nissan <admin@betamark.com>
2025-08-22 12:43:13 -0700

    Add dynamic slicing along Z to chunk manager (#393)
    Summary:
    This PR adds dynamic slicing along Z in the context of the chunk manager.

--- packages/core/examples/chunk_streaming/main.ts
+++ packages/core/examples/chunk_streaming/main.ts
@@ -5,8 +5,10 @@ import {
   OrthographicCamera,
   Region,
 } from "@";
+import { Point } from "@/data/region";
 import { PanZoomControls } from "@/objects/cameras/controls";
 import { ChunkInfoOverlay } from "./chunk_info_overlay";
+import GUI from "lil-gui";
 
 const url =
   "https://public.czbiohub.org/royerlab/zebrahub/imaging/single-objective/ZSNS001.ome.zarr/";
@@ -18,6 +20,7 @@ const bottom = 900;
 // Source is 5D, so provide indices at 3 dimensions to project to 2D.
 // Also specify a subregion in x and y to exercise that part of the API.
 const source = new OmeZarrImageSource(url);
+
 const region: Region = [
   { dimension: "t", index: { type: "point", value: 400 } },
   { dimension: "c", index: { type: "point", value: 0 } },
@@ -25,9 +28,19 @@ const region: Region = [
   { dimension: "y", index: { type: "full" } },
   { dimension: "x", index: { type: "full" } },
 ];
+
+const zIndex = region[2].index as Point;
+
+// values copied from source
+const z = { translate: 0.0, scale: 1.24, shape: 448 };
+const min = z.translate;
+const max = z.translate + z.scale * z.shape - z.scale;
+const zRange = { min, max };
+const gui = new GUI({ width: 500 });
+gui.add(zIndex, "value", zRange.min, zRange.max, z.scale).name("Z-index");
+
 const channelProps = [{ contrastLimits: [0, 255] as [number, number] }];
 const camera = new OrthographicCamera(left, right, top, bottom);
-
 const imageLayer = new ImageLayer({ source, region, channelProps });
 imageLayer.debugMode = true;
 
