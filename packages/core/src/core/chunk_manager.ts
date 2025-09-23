59945631a065042c224097225b12fc5821b09829
Andy Sweet <andrew.d.sweet@gmail.com>
2025-09-23 13:30:49 -0400

    More clean up

--- packages/core/src/core/chunk_manager.ts
+++ packages/core/src/core/chunk_manager.ts
@@ -36,7 +36,6 @@ export class ChunkManagerSource {
   private lastViewBounds2D_: Box2 | null = null;
   private lastZBounds_?: [number, number];
   private lastTCoord_?: number;
-  private fetchedTCoords_: Set<number> = new Set();
 
   constructor(loader: ChunkLoader, sliceCoords: SliceCoordinates) {
     this.loader_ = loader;
@@ -230,7 +229,6 @@ export class ChunkManagerSource {
     const updatedChunks = this.updatePreviousTimeChunks();
 
     const currentTimeChunks = this.chunks_[this.sliceCoords_.t ?? 0];
-    this.fetchedTCoords_.add(this.sliceCoords_.t ?? 0);
     for (const chunk of currentTimeChunks) {
       const isVisible = this.isChunkWithinBounds(chunk, viewBounds3D);
       const eligibleForPrefetch =
