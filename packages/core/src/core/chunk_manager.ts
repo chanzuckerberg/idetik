4c3eceb9c458bd37790d66d79d66443bdcbf8f14
Andy Sweet <andrew.d.sweet@gmail.com>
2025-10-02 12:57:06 -0400

    Fix chunk height

--- packages/core/src/core/chunk_manager.ts
+++ packages/core/src/core/chunk_manager.ts
@@ -107,7 +107,7 @@ export class ChunkManagerSource {
                 orderKey: null,
                 shape: {
                   x: Math.min(chunkWidth, xLod.size - x * chunkWidth),
-                  y: Math.min(chunkWidth, xLod.size - y * chunkWidth),
+                  y: Math.min(chunkHeight, yLod.size - y * chunkHeight),
                   z: Math.min(chunkDepth, (zLod?.size ?? 1) - z * chunkDepth),
                   c: channels,
                 },
