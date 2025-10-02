chanzuckerberg/idetik

commit 4507f58a216c05c43a1c52200ead844dfa342393

Trim padding from edge chunks in OME-Zarr image loader (#470)
### Summary

This PR adds special chunk shape handling for edge chunks in OME-Zarr
datasets. Previously, edge chunks (chunks at the boundaries of an image
that don't align with full chunk dimensions) would receive padded data
from zarrita, which resulted in (sometimes large) areas of zero-valued
padding on the bottom/right of the image (viewing a z slice in standard
orientation).

#### Changes
- `ChunkManager`: Calculate actual chunk dimensions based on remaining
image size for edge chunks
- `OmeZarrImageLoader`: Detect when received chunk data is larger than
expected and remove padding in-place by copying only the valid data rows
  
This does not handle similar issues chunking in `c` or `t`. It seems
unlikely that channels would be chunked in such a way, though I'm not
sure about time. This can also be handled later since the chunk manager
already makes some assumptions about these dimensions.


https://github.com/user-attachments/assets/539e7bae-3094-4169-930b-f7c6061d35fc

### Related Issue
Closes #428

### Tests & Checks
Tested with chunk streaming and OME-Zarr v0.5 examples
Tested with `OmeZarrChunkedImageViewer` react example

packages/core/src/core/chunk_manager.ts
@@ -90,6 +90,7 @@ export class ChunkManagerSource {
 
         for (let x = 0; x < chunksX; ++x) {
           const xOffset = xLod.translation + x * xLod.chunkSize * xLod.scale;
+          const rowStride = Math.min(chunkWidth, xLod.size - x * chunkWidth);
           for (let y = 0; y < chunksY; ++y) {
             const yOffset = yLod.translation + y * yLod.chunkSize * yLod.scale;
             for (let z = 0; z < chunksZ; ++z) {
@@ -105,12 +106,12 @@ export class ChunkManagerSource {
                 priority: null,
                 orderKey: null,
                 shape: {
-                  x: chunkWidth,
-                  y: chunkHeight,
-                  z: chunkDepth,
+                  x: Math.min(chunkWidth, xLod.size - x * chunkWidth),
+                  y: Math.min(chunkWidth, xLod.size - y * chunkWidth),
+                  z: Math.min(chunkDepth, (zLod?.size ?? 1) - z * chunkDepth),
                   c: channels,
                 },
-                rowStride: chunkWidth,
+                rowStride,
                 rowAlignmentBytes: 1,
                 chunkIndex: { x, y, z, t },
                 scale: {

packages/core/src/data/ome_zarr/image_loader.ts
@@ -10,6 +10,8 @@ import {
   isChunkData,
   LoaderAttributes,
   SliceCoordinates,
+  ChunkData,
+  ChunkDataConstructor,
 } from "../chunk";
 import { isTextureUnpackRowAlignment } from "../../objects/textures/texture";
 import { PromiseScheduler } from "../promise_scheduler";
@@ -94,27 +96,85 @@ export class OmeZarrImageLoader {
 
     const array = this.arrays_[chunk.lod];
     const arrayParams = this.arrayParams_[chunk.lod];
-    const subarray = await getChunk(array, arrayParams, chunkCoords, {
+    const receivedChunk = await getChunk(array, arrayParams, chunkCoords, {
       signal,
     });
 
-    const data = subarray.data;
-    if (!isChunkData(data)) {
+    if (!isChunkData(receivedChunk.data)) {
       throw new Error(
-        `Subarray has an unsupported data type, data=${data.constructor.name}`
+        `Received chunk has an unsupported data type, data=${receivedChunk.data.constructor.name}`
       );
     }
 
-    const rowAlignment = data.BYTES_PER_ELEMENT;
+    const receivedShape = {
+      x: receivedChunk.shape[this.dimensions_.x.index],
+      y: receivedChunk.shape[this.dimensions_.y.index],
+      z: this.dimensions_.z
+        ? receivedChunk.shape[this.dimensions_.z.index]
+        : chunk.shape.z,
+    };
+
+    const receivedChunkTooSmall =
+      receivedShape.x < chunk.shape.x ||
+      receivedShape.y < chunk.shape.y ||
+      receivedShape.z < chunk.shape.z;
+
+    if (receivedChunkTooSmall) {
+      throw new Error(
+        `Received incompatible shape for chunkIndex ${JSON.stringify(chunk.chunkIndex)} at LOD ${chunk.lod}: ` +
+          `expected shape: ${JSON.stringify(chunk.shape)}, received shape: ${JSON.stringify(receivedShape)} (too small)`
+      );
+    }
+
+    const receivedChunkHasPadding =
+      receivedShape.x > chunk.shape.x ||
+      receivedShape.y > chunk.shape.y ||
+      receivedShape.z > chunk.shape.z;
+
+    if (receivedChunkHasPadding) {
+      chunk.data = this.trimChunkPadding(
+        chunk,
+        receivedChunk.data,
+        receivedChunk.stride
+      );
+    } else {
+      chunk.data = receivedChunk.data;
+      chunk.rowStride = receivedChunk.stride[this.dimensions_.y.index];
+    }
+
+    const rowAlignment = chunk.data.BYTES_PER_ELEMENT;
     if (!isTextureUnpackRowAlignment(rowAlignment)) {
       throw new Error(
         "Invalid row alignment value. Possible values are 1, 2, 4, or 8"
       );
     }
-
     chunk.rowAlignmentBytes = rowAlignment;
-    chunk.rowStride = subarray.stride[this.dimensions_.y.index];
-    chunk.data = data;
+  }
+
+  private trimChunkPadding(
+    chunk: Chunk,
+    receivedChunkData: ChunkData,
+    receivedChunkStride: number[]
+  ): ChunkData {
+    const compactSize = chunk.shape.x * chunk.shape.y * chunk.shape.z;
+    const compactData =
+      new (receivedChunkData.constructor as ChunkDataConstructor)(compactSize);
+
+    let offset = 0;
+    const zStride = this.dimensions_.z
+      ? receivedChunkStride[this.dimensions_.z.index]
+      : 0;
+    const yStride = receivedChunkStride[this.dimensions_.y.index];
+    for (let z = 0; z < chunk.shape.z; z++) {
+      const zStart = z * zStride;
+      for (let y = 0; y < chunk.shape.y; y++) {
+        const srcStart = zStart + y * yStride;
+        const srcEnd = srcStart + chunk.shape.x;
+        compactData.set(receivedChunkData.subarray(srcStart, srcEnd), offset);
+        offset += chunk.shape.x;
+      }
+    }
+    return compactData;
   }
 
   public async loadRegion(

commit 22ddafe61c8aba46ef7c58f852547331262aa33a

Fix chunk height in chunk manager initialization (#472)
### Summary
This changes fixes setting the chunk height, which was broken in
https://github.com/chanzuckerberg/idetik/pull/470.

### Tests & Checks
I tested the main chunk streaming example.

packages/core/src/core/chunk_manager.ts
@@ -107,7 +107,7 @@ export class ChunkManagerSource {
                 orderKey: null,
                 shape: {
                   x: Math.min(chunkWidth, xLod.size - x * chunkWidth),
-                  y: Math.min(chunkWidth, xLod.size - y * chunkWidth),
+                  y: Math.min(chunkHeight, yLod.size - y * chunkHeight),
                   z: Math.min(chunkDepth, (zLod?.size ?? 1) - z * chunkDepth),
                   c: channels,
                 },