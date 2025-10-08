a9fc9a3945fe458f56cc5aa4b13f1e01a25c8720
Andy Sweet <andrew.d.sweet@gmail.com>
2025-10-08 16:06:08 -0400

    Fix pydetik buil

--- packages/pydetik/src/pydetik/_version.py
+++ packages/pydetik/src/pydetik/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '0.1.dev0+d20251002'
-__version_tuple__ = version_tuple = (0, 1, 'dev0', 'd20251002')
+__version__ = version = '5.0.1.dev21+g616df6b31.d20251008'
+__version_tuple__ = version_tuple = (5, 0, 1, 'dev21', 'g616df6b31.d20251008')
 
-__commit_id__ = commit_id = None
+__commit_id__ = commit_id = 'g616df6b31'
