965b876c3157969f1bbfa4ab959a7eae3c8f34cd
Andy Sweet <andrew.d.sweet@gmail.com>
2025-10-21 16:25:16 -0400

    Add cell observatory example

---
+++ packages/core/examples/cell_observatory/main.ts
@@ -0,0 +1,178 @@
+import {
+  ChannelProps,
+  Idetik,
+  ChunkedImageLayer,
+  OmeZarrImageSource,
+  OrthographicCamera,
+  createPlaybackPolicy,
+  createNoPrefetchPolicy,
+} from "@";
+import { PanZoomControls } from "@/objects/cameras/controls";
+import { ChunkInfoOverlay } from "../chunk_info_overlay";
+import { addDimensionSlider } from "../lil_gui_utils";
+import GUI from "lil-gui";
+
+// Locally hosted s3 bucket: czi-dynamic-cell-atlas-staging
+const url =
+  "http://localhost:8000/cell-observatory/2025-10-17/2025/10/1/20250211_mem_histone/fish7_48hpf_mYH/roi1/000x_000y_000z.zarr";
+
+// Source is 5D, so provide indices at 3 dimensions to project to 2D.
+const source = new OmeZarrImageSource(url);
+
+const loader = await source.open();
+const dimensions = loader.getSourceDimensionMap();
+const x = dimensions.x.lods[0];
+const y = dimensions.y.lods[0];
+const z = dimensions.z!.lods[0];
+const t = dimensions.t!.lods[0];
+
+const imageDataRange = { min: 0, max: 2048 };
+
+const zMin = z.translation;
+const zMax = z.translation + z.scale * z.size - z.scale;
+const zRange = { min: zMin, max: zMax };
+
+const tMin = t.translation;
+const tMax = t.translation + t.scale * t.size - t.scale;
+const tRange = { min: tMin, max: tMax };
+
+const left = x.translation;
+const right = x.translation + x.scale * x.size;
+const top = y.translation;
+const bottom = y.translation + y.scale * y.size;
+
+const initialWindow = 50;
+const initialLevel = 50;
+const initialContrastLimits = windowLevelToContrastLimits(
+  initialWindow,
+  initialLevel
+);
+const channelProps: ChannelProps[] = [
+  { contrastLimits: initialContrastLimits },
+];
+
+const sliceCoords = {
+  t: 0.5 * (tMin + tMax),
+  z: 0.5 * (zMin + zMax),
+  c: 0,
+};
+
+const camera = new OrthographicCamera(left, right, top, bottom);
+const imageLayer = new ChunkedImageLayer({ source, sliceCoords, policy: createNoPrefetchPolicy(), channelProps });
+imageLayer.debugMode = true;
+
+const overlaySelector = document.querySelector<HTMLDivElement>("#chunk-info")!;
+const chunkInfoOverlay = new ChunkInfoOverlay({
+  textDiv: overlaySelector,
+  imageLayer: imageLayer,
+});
+
+const timePointDiv = document.querySelector<HTMLDivElement>("#time-point")!;
+const timePointOverlay = {
+  update(_idetik: Idetik, _timestamp?: DOMHighResTimeStamp) {
+    const time = imageLayer.lastPresentationTimeCoord;
+    timePointDiv.textContent = `t = ${time}`;
+  },
+};
+
+new Idetik({
+  canvas: document.querySelector<HTMLCanvasElement>("#canvas")!,
+  camera,
+  cameraControls: new PanZoomControls(camera),
+  layers: [imageLayer],
+  overlays: [chunkInfoOverlay, timePointOverlay],
+  showStats: true,
+}).start();
+
+const controls = {
+  showWireframes: imageLayer.debugMode,
+  showChunkInfoOverlay: true,
+  showTimePointOverlay: true,
+  window: initialWindow,
+  level: initialLevel,
+  resetContrast: function () {
+    contrastFolder.reset();
+  },
+};
+
+const gui = new GUI({ width: 500 });
+
+addDimensionSlider({
+  gui,
+  sliceCoords,
+  dimensionName: "z",
+  minValue: zRange.min,
+  maxValue: zRange.max,
+  stepValue: z.scale,
+  playback: {},
+});
+
+addDimensionSlider({
+  gui,
+  sliceCoords,
+  dimensionName: "t",
+  minValue: tRange.min,
+  maxValue: tRange.max,
+  stepValue: t.scale,
+  playback: {
+    onRateChange: (rateHz: number) => {
+      imageLayer.imageSourcePolicy =
+        rateHz > 0 ? createPlaybackPolicy() : createNoPrefetchPolicy ();
+    },
+  },
+});
+
+const overlaysFolder = gui.addFolder("Overlays");
+
+overlaysFolder
+  .add(controls, "showTimePointOverlay")
+  .name("Show time point overlay")
+  .onChange((show: boolean) => {
+    timePointDiv.style.display = show ? "block" : "none";
+  });
+
+overlaysFolder
+  .add(controls, "showWireframes")
+  .name("Show tile wireframes")
+  .onChange((show: boolean) => (imageLayer.debugMode = show));
+
+overlaysFolder
+  .add(controls, "showChunkInfoOverlay")
+  .name("Show chunk information overlay")
+  .onChange((show: boolean) => {
+    overlaySelector.style.display = show ? "block" : "none";
+  });
+
+const contrastFolder = gui.addFolder("Window/Level");
+contrastFolder
+  .add(controls, "window", 1, 100, 1)
+  .name("Window (%)")
+  .onChange(updateContrastLimits);
+
+contrastFolder
+  .add(controls, "level", 0, 100, 1)
+  .name("Level (%)")
+  .onChange(updateContrastLimits);
+
+contrastFolder.add(controls, "resetContrast").name("Reset");
+
+function updateContrastLimits() {
+  const contrastLimits = windowLevelToContrastLimits(
+    controls.window,
+    controls.level
+  );
+  const newChannelProps = [{ contrastLimits }];
+  imageLayer.setChannelProps(newChannelProps);
+}
+
+function windowLevelToContrastLimits(
+  window: number,
+  level: number
+): [number, number] {
+  return [
+    (imageDataRange.max - imageDataRange.min) *
+      (level / 100 - window / 100 / 2),
+    (imageDataRange.max - imageDataRange.min) *
+      (level / 100 + window / 100 / 2),
+  ];
+}
--- packages/pydetik/src/pydetik/_version.py
+++ packages/pydetik/src/pydetik/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '6.0.1.dev27+gb18f48692.d20251020'
-__version_tuple__ = version_tuple = (6, 0, 1, 'dev27', 'gb18f48692.d20251020')
+__version__ = version = '6.0.1.dev30+g2ca3844a1.d20251021'
+__version_tuple__ = version_tuple = (6, 0, 1, 'dev30', 'g2ca3844a1.d20251021')
 
-__commit_id__ = commit_id = 'gb18f48692'
+__commit_id__ = commit_id = 'g2ca3844a1'
