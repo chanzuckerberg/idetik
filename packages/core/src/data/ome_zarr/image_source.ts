bac3107466368e33aa3a800af43bc15c8e8329f1
Andy Sweet <andrew.d.sweet@gmail.com>
2025-10-24 16:14:32 -0400

    Pass version in construction

--- packages/core/src/data/ome_zarr/image_source.ts
+++ packages/core/src/data/ome_zarr/image_source.ts
@@ -17,27 +17,29 @@ import {
 /** Opens an OME-Zarr multiscale image Zarr group from either a URL or local directory. */
 export class OmeZarrImageSource {
   readonly location: Location<Readable>;
+  readonly version?: OmeZarrVersion;
 
   /**
    * @param url URL of Zarr root
    */
-  constructor(url: string);
+  constructor(url: string, version?: OmeZarrVersion);
   /**
    * @param directory return value of `window.showDirectoryPicker()` which gives the browser
    *    permission to access a directory (only works in Chrome/Edge)
    * @param path path to image, beginning with "/". This argument allows the application to only
    *    ask the user once for permission to the root directory
    */
-  constructor(directory: FileSystemDirectoryHandle, path?: `/${string}`);
-  constructor(source: string | FileSystemDirectoryHandle, path?: `/${string}`) {
+  constructor(directory: FileSystemDirectoryHandle, version?: OmeZarrVersion, path?: `/${string}`);
+  constructor(source: string | FileSystemDirectoryHandle, version?: OmeZarrVersion, path?: `/${string}`) {
     this.location =
       typeof source === "string"
         ? new Location(new FetchStore(source))
         : new Location(new WebFileSystemStore(source), path);
+    this.version = version;
   }
 
-  public async open(version?: OmeZarrVersion): Promise<OmeZarrImageLoader> {
-    let zarrVersion = version ? omeZarrToZarrVersion(version) : undefined;
+  public async open(): Promise<OmeZarrImageLoader> {
+    let zarrVersion = this.version ? omeZarrToZarrVersion(this.version) : undefined;
     const root = await openGroup(this.location, zarrVersion);
     const adaptedOmeImage = parseOmeZarrImage(root.attrs);
     const images = adaptedOmeImage.multiscales;
