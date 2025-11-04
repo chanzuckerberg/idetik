06ade6278df9a9eacac927c92264414f92eede6c
Sean Martin <martins7@tcd.ie>
2025-11-04 18:34:08 +0100

    fix: correct problems from main merge

--- packages/core/src/objects/textures/texture_3d.ts
+++ packages/core/src/objects/textures/texture_3d.ts
@@ -88,9 +88,6 @@ export class Texture3D extends Texture {
       chunk.shape.y,
       chunk.shape.z
     );
-    // TODO (SKM): here is where we'd store the columnStride if needed
-    // for 3D textures.
-    texture.unpackRowLength = chunk.rowStride;
     texture.unpackAlignment = chunk.rowAlignmentBytes;
     return texture;
   }
