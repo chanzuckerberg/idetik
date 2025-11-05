434059e17b1cfed0e20bc84163c4f8775527bec7
Andy Sweet <andrew.d.sweet@gmail.com>
2025-11-05 16:44:34 -0500

    Fix disposal bug

--- packages/core/src/core/chunk_manager_source.ts
+++ packages/core/src/core/chunk_manager_source.ts
@@ -357,7 +357,7 @@ export class ChunkManagerSource {
           isFallbackLOD,
           isCurrentLOD,
           isVisible,
-          chunk.prefetch,
+          eligibleForPrefetch,
           isChannelInSlice
         )
       ) {
