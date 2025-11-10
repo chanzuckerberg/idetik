9735714b13ecd97ffaa4674871f86e6ff4886bd8
Sean Martin <martins7@tcd.ie>
2025-11-10 10:02:01 +0100

    feat: volume renderable structure and volume shader

--- packages/core/src/objects/renderable/volume_renderable.ts
+++ packages/core/src/objects/renderable/volume_renderable.ts
@@ -1,14 +1,38 @@
+import { Shader } from "../../renderers/shaders";
 import { RenderableObject } from "../../core/renderable_object";
 import { BoxGeometry } from "../geometry/box_geometry";
+import { TextureDataType } from "../textures/texture";
+import { Texture3D } from "../textures/texture_3d";
 
 export class VolumeRenderable extends RenderableObject {
-  constructor() {
+  constructor(
+    width: number,
+    height: number,
+    depth: number,
+    texture: Texture3D
+  ) {
     super();
-    this.geometry = new BoxGeometry(1, 1, 1, 1, 1, 1);
-    this.programName = "volume";
+    this.geometry = new BoxGeometry(width, height, depth, 1, 1, 1);
+    this.setTexture(0, texture);
+    this.programName = dataTypeToImageShader(texture.dataType);
   }
 
   public get type() {
     return "VolumeRenderable";
+  }
+}
+
+function dataTypeToImageShader(dataType: TextureDataType): Shader {
+  switch (dataType) {
+    case "byte":
+    case "int":
+    case "short":
+      return "intVolumeImage";
+    case "unsigned_short":
+    case "unsigned_byte":
+    case "unsigned_int":
+      return "uintVolumeImage";
+    case "float":
+      return "floatVolumeImage";
   }
 }
--- packages/core/src/renderers/shaders/volume_frag.glsl
+++ packages/core/src/renderers/shaders/volume_frag.glsl
@@ -1,9 +1,29 @@
 #version 300 es
+#pragma inject_defines
 
 precision mediump float;
 
 layout (location = 0) out vec4 fragColor;
 
+#if defined TEXTURE_DATA_TYPE_INT
+uniform mediump isampler3D ImageSampler;
+#elif defined TEXTURE_DATA_TYPE_UINT
+uniform mediump usampler3D ImageSampler;
+#else
+uniform mediump sampler3D ImageSampler;
+#endif
+
+in vec2 TexCoords;
+
 void main() {
-    fragColor = vec4(0.6, 0.6, 0.6, 1.0);
+    float alpha = 0.0;
+    /* Will replace fixed steps and normalization with uniforms later */
+    for (int i = 0; i < 256; i++) {
+        float z = float(i) / 255.0;
+        float texel = float(texture(ImageSampler, vec3(TexCoords, z)).r);
+        float newAlpha = clamp(texel / 1000.0, 0.0, 1.0);
+        alpha = (1.0 - alpha) * newAlpha + alpha;
+    }
+    float displayValue = alpha;
+    fragColor = vec4(displayValue, displayValue, displayValue, 1.0);
 }
