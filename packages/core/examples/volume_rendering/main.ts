fd4e1de0c32d0eb7b6c26006dd42b16f37845659
Sean Martin <martins7@tcd.ie>
2025-12-19 13:23:05 +0100

    refactor: clean up volume example controls

--- packages/core/examples/volume_rendering/main.ts
+++ packages/core/examples/volume_rendering/main.ts
@@ -46,15 +46,8 @@ const idetik = new Idetik({
 
 idetik.start();
 
-const controls = {
-  showWireframes: volumeLayer.debugMode,
-  showTimePointOverlay: true,
-  enableRayCorrection: false,
-  lod: 2,
-};
-
+// Add GUI controls to manipulate rendering
 const gui = new GUI({ width: 500 });
-
 addDimensionSlider({
   gui,
   sliceCoords,
@@ -69,45 +62,22 @@ addDimensionSlider({
     },
   },
 });
-
-const overlaysFolder = gui.addFolder("Overlays");
-
-overlaysFolder
-  .add(controls, "showWireframes")
-  .name("Show tile wireframes")
-  .onChange((show: boolean) => {
-    volumeLayer.debugMode = show;
-    controls.showWireframes = show;
-  });
-
-overlaysFolder
-  .add(controls, "enableRayCorrection")
-  .name("Enable Ray Correction at Bounds")
-  .onChange((show: boolean) => {
-    controls.enableRayCorrection = show;
-    volumeLayer.enableRayCorrection = show;
-  });
+gui.add(volumeLayer, "lod", 0, 2, 1).name("Level of Detail (LOD)");
 
 const volumeFolder = gui.addFolder("Volume Rendering");
-
-volumeFolder
-  .add(controls, "lod", 0, 2, 1)
-  .name("Level of Detail (LOD)")
-  .onChange((lod: number) => {
-    volumeLayer.lod = lod;
-    controls.lod = lod;
-  });
-
 volumeFolder
   .add(volumeLayer, "sampleDensity", 16, 512, 1)
-  .name("Sample Density");
-
-volumeFolder.add(volumeLayer, "maxIntensity", 1, 255, 1).name("Max Intensity");
-
+  .name("Sample density");
+volumeFolder.add(volumeLayer, "maxIntensity", 1, 255, 1).name("Max intensity");
 volumeFolder
   .add(volumeLayer, "opacityScale", 0.01, 1.0, 0.01)
-  .name("Opacity Scale");
-
+  .name("Opacity scale");
 volumeFolder
-  .add(volumeLayer, "alphaThreshold", 0.5, 1.0, 0.01)
-  .name("Alpha Threshold");
+  .add(volumeLayer, "alphaThreshold", 0.8, 1.0, 0.01)
+  .name("Early termination threshold");
+
+const overlaysFolder = gui.addFolder("Debug");
+overlaysFolder.add(volumeLayer, "debugMode").name("Show tile wireframes");
+overlaysFolder
+  .add(volumeLayer, "showEmptyRays")
+  .name("Show rays with length 0");
--- packages/core/src/layers/volume_layer.ts
+++ packages/core/src/layers/volume_layer.ts
@@ -33,7 +33,7 @@ export class VolumeLayer extends Layer {
 
   private lastLoadedLod_ = -1;
   private lastLoadedTime_ = -1;
-  public enableRayCorrection = false;
+  public showEmptyRays = false;
   private color_ = vec3.fromValues(1.0, 1.0, 1.0);
   public sampleDensity = 128.0; // Samples per unit texture space
   public maxIntensity = 255.0; // Normalization factor for intensity
@@ -239,7 +239,7 @@ export class VolumeLayer extends Layer {
 
   public getUniforms(): Record<string, unknown> {
     return {
-      EnableRayCorrection: Number(this.enableRayCorrection),
+      ShowEmptyRays: Number(this.showEmptyRays),
       SampleDensity: this.sampleDensity,
       MaxIntensity: this.maxIntensity,
       OpacityScale: this.opacityScale,
