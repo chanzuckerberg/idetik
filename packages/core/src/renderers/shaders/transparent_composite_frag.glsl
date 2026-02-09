e7228d06e573ff1afe147fe8fd557f0c9ab1164b
aranega <vincent.aranega@gmail.com>
2026-02-09 09:25:24 -0600

    fix: Reintroduce special blending mode for OIT

--- packages/core/src/renderers/shaders/transparent_composite_frag.glsl
+++ packages/core/src/renderers/shaders/transparent_composite_frag.glsl
@@ -5,7 +5,7 @@ precision highp float;
 // version, not the single blend mode version
 uniform sampler2D AccumSampler;
 uniform sampler2D RevealSampler;
-//uniform sampler2D DepthSampler;
+uniform sampler2D DepthSampler;
 
 in vec2 TexCoords;
 layout (location = 0) out vec4 fragColor;
@@ -15,6 +15,6 @@ void main() {
     vec4 tex1 = texture(RevealSampler, TexCoords);
     vec4 accum = vec4(tex0.rgb, tex1.r);
     float revealage = tex0.a;
-    //float depth = texture(DepthSampler, TexCoords).r;
-    fragColor = clamp(vec4(accum.rgb / clamp(accum.a, 1e-4, 3e4), revealage), 0.0, 1.0);
+    float depth = texture(DepthSampler, TexCoords).r;
+    fragColor = clamp(vec4(accum.rgb / clamp(accum.a * depth, 1e-4, 3e4), revealage), 0.0, 1.0);
 }
