28fd8a0b8129f8510746e98842ee4c60defc25f2
Sean Martin <martins7@tcd.ie>
2026-02-17 13:09:42 +0100

    feat: new example for multi-chan volume rendering

---
+++ packages/core/examples/volume_rendering_multi_channel/main.ts
@@ -0,0 +1,111 @@
+import { Idetik, VolumeLayer, PerspectiveCamera, OmeZarrImageSource } from "@";
+import { OrbitControls } from "@/objects/cameras/orbit_controls";
+import { createPlaybackPolicy } from "@/core/image_source_policy";
+import { addDimensionSlider } from "../lil_gui_utils";
+import GUI from "lil-gui";
+import { vec3 } from "gl-matrix";
+
+const url =
+  "https://public.czbiohub.org/organelle_box/datasets/A549/organelle_box_crop_v1.zarr/CLTA/PFA/002000/";
+const source = OmeZarrImageSource.fromHttp({ url });
+const sliceCoords = {
+  t: 0,
+  z: undefined,
+  c: undefined, // Show all channels
+};
+const controls = { lod: 0 };
+const channelProps = [
+  {
+    visible: false,
+    color: [1, 1, 1] as [number, number, number],
+    contrastLimits: [-1.5, 10.0] as [number, number],
+  },
+  {
+    visible: true,
+    color: [0, 0, 1] as [number, number, number],
+    contrastLimits: [108, 353] as [number, number],
+  },
+  {
+    visible: true,
+    color: [0, 1, 0] as [number, number, number],
+    contrastLimits: [144, 3825] as [number, number],
+  },
+];
+
+const camera = new PerspectiveCamera();
+const policy = createPlaybackPolicy({
+  lod: { min: controls.lod, max: controls.lod },
+});
+const volumeLayer = new VolumeLayer({
+  source,
+  sliceCoords,
+  policy,
+  channelProps,
+});
+const idetik = new Idetik({
+  canvas: document.querySelector<HTMLCanvasElement>("#canvas")!,
+  viewports: [
+    {
+      camera,
+      cameraControls: new OrbitControls(camera, {
+        radius: 400,
+        target: vec3.fromValues(0, 0, -100), // Volume center,
+      }),
+      layers: [volumeLayer],
+    },
+  ],
+  showStats: true,
+});
+
+idetik.start();
+
+// Add GUI controls to manipulate rendering
+const gui = new GUI({ width: 500 });
+addDimensionSlider({
+  gui,
+  sliceCoords,
+  dimensionName: "t",
+  minValue: 0,
+  maxValue: 800,
+  stepValue: 1.0,
+  playback: {},
+});
+gui
+  .add(controls, "lod", 0, 2, 1)
+  .name("Level of Detail (LOD)")
+  .onChange(
+    (lod: number) =>
+      (volumeLayer.sourcePolicy = createPlaybackPolicy({
+        lod: { min: lod, max: lod },
+      }))
+  );
+
+const volumeFolder = gui.addFolder("Volume Rendering");
+volumeFolder
+  .add(volumeLayer, "relativeStepSize", 0.25, 3.0, 0.1)
+  .name("Relative step size (voxels)");
+
+// maps 0-1 slider to [0.001, 10.0] logarithmically
+const opacityControls = {
+  get opacity() {
+    return (Math.log10(volumeLayer.opacityMultiplier) + 3) / 4;
+  },
+  set opacity(sliderValue: number) {
+    volumeLayer.opacityMultiplier = Math.pow(10, sliderValue * 4 - 3);
+  },
+};
+volumeFolder
+  .add(opacityControls, "opacity", 0, 1, 0.01)
+  .name("Opacity")
+  .decimals(2);
+volumeFolder
+  .add(volumeLayer, "earlyTerminationAlpha", 0.8, 1.0, 0.01)
+  .name("Early termination threshold");
+
+const overlaysFolder = gui.addFolder("Debug");
+overlaysFolder
+  .add(volumeLayer, "debugShowWireframes")
+  .name("Show tile wireframes");
+overlaysFolder
+  .add(volumeLayer, "debugShowDegenerateRays")
+  .name("Show degenerate rays (length 0)");
