a24f62403585d7aec5fbec8e83376f975e10a67e
Shlomi Nissan <admin@betamark.com>
2026-04-14 07:31:11 -0700

    Add pipeline factory

--- src/renderers/webgpu/webgpu_pipelines.ts
+++ src/renderers/webgpu/webgpu_pipelines.ts
@@ -1,11 +1,110 @@
-import WebGPUShaderLibrary from "./webgpu_shader_library";
+import WebGPUShaderLibrary, { ShaderName } from "./webgpu_shader_library";
+
+export type PipelineKey = {
+  shaderName: ShaderName;
+  depthWrite: boolean;
+  depthTest: boolean;
+  stencil: boolean;
+  cullMode: GPUCullMode;
+  topology: GPUPrimitiveTopology;
+};
+
+type WebGPUPipeline = {
+  key: PipelineKey;
+  pipeline: GPURenderPipeline;
+};
 
 export default class WebGPUPipelines {
+  private readonly colorFormat_: GPUTextureFormat;
+  private readonly depthFormat_: GPUTextureFormat;
   private readonly device_: GPUDevice;
+  private readonly pipelines_: WebGPUPipeline[];
   private readonly shaderLibrary_: WebGPUShaderLibrary;
 
-  constructor(device: GPUDevice, shaderLibrary: WebGPUShaderLibrary) {
+  constructor(
+    device: GPUDevice,
+    shaderLibrary: WebGPUShaderLibrary,
+    colorFormat: GPUTextureFormat,
+    depthFormat: GPUTextureFormat
+  ) {
+    this.colorFormat_ = colorFormat;
+    this.depthFormat_ = depthFormat;
     this.device_ = device;
+    this.pipelines_ = [];
     this.shaderLibrary_ = shaderLibrary;
+  }
+
+  public get(key: PipelineKey): GPURenderPipeline {
+    const cached = this.getCachedPipeline(key);
+    if (cached) return cached.pipeline;
+
+    const shader = this.shaderLibrary_.get(key.shaderName);
+    const layout = this.device_.createPipelineLayout({
+      bindGroupLayouts: shader.bindGroupLayouts,
+    });
+
+    const depthCompare: GPUCompareFunction = key.depthTest
+      ? "less-equal"
+      : "always";
+
+    const stencilFace: GPUStencilFaceState = key.stencil
+      ? { compare: "always", passOp: "replace" }
+      : {};
+
+    const pipeline = this.device_.createRenderPipeline({
+      layout,
+      vertex: {
+        module: shader.module,
+        entryPoint: "vert",
+        buffers: [
+          {
+            attributes: [
+              {
+                shaderLocation: 0,
+                offset: 0,
+                format: "float32x3",
+              },
+            ],
+            arrayStride: 4 * 3,
+            stepMode: "vertex",
+          },
+        ],
+      },
+      fragment: {
+        module: shader.module,
+        entryPoint: "frag",
+        targets: [{ format: this.colorFormat_ }],
+      },
+      primitive: {
+        topology: key.topology,
+        frontFace: "ccw",
+        cullMode: key.cullMode,
+      },
+      depthStencil: {
+        format: this.depthFormat_,
+        depthWriteEnabled: key.depthWrite,
+        depthCompare,
+        stencilFront: stencilFace,
+        stencilBack: stencilFace,
+        stencilReadMask: 0xff,
+        stencilWriteMask: key.stencil ? 0xff : 0x00,
+      },
+    });
+
+    this.pipelines_.push({ key, pipeline });
+
+    return pipeline;
+  }
+
+  private getCachedPipeline(key: PipelineKey) {
+    return this.pipelines_.find(
+      (p) =>
+        p.key.shaderName === key.shaderName &&
+        p.key.depthWrite === key.depthWrite &&
+        p.key.depthTest === key.depthTest &&
+        p.key.stencil === key.stencil &&
+        p.key.cullMode === key.cullMode &&
+        p.key.topology === key.topology
+    );
   }
 }
