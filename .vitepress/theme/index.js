81b1e2fd98fa2f435d6532dd5647a30c3f5a92fc
Shlomi Nissan <admin@betamark.com>
2026-08-13 08:48:26 -0700

    docs: add github star counter to navbar

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,9 +1,16 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import VersionBadge from './VersionBadge.vue'
+import GitHubStars from './GitHubStars.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-after': () => h(GitHubStars),
+    })
+  },
   enhanceApp({ app }) {
     app.component('VersionBadge', VersionBadge)
   },
